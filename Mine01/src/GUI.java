2fd9fc92b1240f943fcda4864c5382e5f7550e74
Nourankandar <147334965+Nourankandar@users.noreply.github.com>
2023-10-13 21:17:16 +0300

    no

--- Mine01/src/GUI.java
+++ Mine01/src/GUI.java
@@ -1,14 +1,21 @@
 import javax.swing.*;
 
 public class GUI extends JFrame {
-
+    PaneL paneL = new PaneL();
     public GUI(){
-        setSize(600,400);
+        setSize(618,620);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setTitle("MineSweeper");
         setLocation(500, 200);
         setLayout(null);
+        paneL.setBounds(2,100,600,480);
+        //setBounds(2,2,600,40)
+        //setBounds(2,42,600,98)
+
+        paneL.panel3();
+        add(paneL);
+
         setVisible(true);
 
     }
--- Mine01/src/PaneL.java
+++ Mine01/src/PaneL.java
@@ -2,6 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PaneL extends JPanel {
+    Button button[][]= new Button[20][20];
 
+    public void panel3() {
+        setLayout(new GridLayout(12,12));
+        for(int i =0 ; i<12 ; i++){
+            for(int j=0;j<12;j++) {
+              button[i][j] = new Button();
+              add(button[i][j]);
+              setBackground(new Color(176, 211, 232));
+
+
+            }
+        }
     }
+}
 
