dc47b7f83f50abbb56c9ca0fd3e112fdb01d29e2
Nourankandar <147334965+Nourankandar@users.noreply.github.com>
2023-10-13 19:41:27 +0300

    no

--- Mine01/src/GUI.java
+++ Mine01/src/GUI.java
@@ -2,4 +2,14 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 
+    public GUI(){
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setTitle("MineSweeper");
+        setLocation(500, 200);
+        setLayout(null);
+        setVisible(true);
+
+    }
 }
